b175d0695fe7be19011277494406a74500cc9683
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2018-11-19 23:56:29 -0300

    updated

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import *
+from .models import category, obra, pessoa, pessoaVoto, voto
 
 admin.site.register(category)
-admin.site.register(obra)
+
+class obraAdmin(admin.ModelAdmin):
+    list_display = ('insc_obra', 'nome_obra', 'category_obra',)
+    list_filter = ('category_obra',)
+
+
+admin.site.register(obra, obraAdmin)
 admin.site.register(pessoa)
 admin.site.register(pessoaVoto)
-admin.site.register(voto)
+
+class votoAdmin(admin.ModelAdmin):
+    list_display = ('obraVotada', 'quantidadeVotos', 'category_obra',)
+    list_filter = ('quantidadeVotos','category_obra',)
+
+
+admin.site.register(voto, votoAdmin)
